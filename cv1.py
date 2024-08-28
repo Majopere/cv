5d9ff897590c0a2b6520bf5f3cbe5bf58d29e856
Majopere <majoperegam@gmail.com>
2024-08-27 18:08:10 -0600

    Added initial files

---
+++ cv1.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+st.markdown("# Mi Currículum Vitae")
+
+st.markdown("## Información Personal")
+st.markdown("""
+- *Nombre:* María José Peregrina  
+- *Email:* majoperegam@gmail.com  
+- *Teléfono:* 3316027128
+""")
+
+st.markdown("## Experiencia Laboral")
+st.markdown("""
+- *Trabajo 1:* Asistente personal de arquitecta  
+  - *Fecha:* 2017-2021  
+  - *Descripción:* Llevar agenda y asistencia en general.
+
+- *Trabajo 2:* Start Up  
+  - *Fecha:* 2017-2019  
+  - *Descripción:* Diseño, venta y distribución de accesorios para joyería.
+""")
+
+st.markdown("## Educación")
+st.markdown("""
+- *Título:* Lic. Administración y Finanzas  
+  - *Institución:* Universidad Panamericana  
+  - *Fecha:* 2020 - presente
+
+- *Título:* Intercambio universitario  
+  - *Institución:* Université Jean Moulin Lyon 3  
+  - *Fecha:* 2023
+""")
+
+st.markdown("## Habilidades")
+st.markdown("""
+- Programación en Python
+- Diseño de Interfaces
+- Trabajo en equipo
+- Manejo de Excel
+- Idiomas: inglés y francés
+- Puntualidad
+""")
